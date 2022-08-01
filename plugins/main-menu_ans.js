3f101a16194982f9b199c6664f42f14b8f83fcbb
Geeruu0 <110067064+Geeruu0@users.noreply.github.com>
2022-08-01 19:34:50 +0700

    Update main-menu_ans.js

--- plugins/main-menu_ans.js
+++ plugins/main-menu_ans.js
@@ -2,7 +2,7 @@ const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessa
 let fs = require('fs')
 let handler = async (m) => {
 let krizyn = `
-*Hallo* %user Apa Kabar Baik Kan Kamu %user?
+*Hallo* %name Apa Kabar Baik Kan Kamu %name?
 
 	`.trim()
   let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya5)).buffer()}, { upload: conn.waUploadToServer }) 
